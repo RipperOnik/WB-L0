02eee9e67bf8557720ad29469609283a698fa6b2
RipperOnik <kingstrong2@mail.ru>
2022-11-24 19:53:04 +0300

    added payment form with functionality and popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,4 @@
+// delivery
 const deliveryPopupContainer = document.getElementById("delivery-popup-container")
 const deliveryPopupButton = document.getElementById("delivery-popup-button")
 const deliveryCloseButton = document.getElementById("delivery-popup-close-button")
@@ -5,20 +6,36 @@ const tabIssue = document.getElementById("issue-points-tab")
 const tabCourier = document.getElementById("courier-tab")
 const addressField = document.getElementById("address")
 const ratingField = document.getElementById("rating")
-const deliverybuttonOk = document.getElementById("button-ok")
-function togglePopup() {
+const deliverybuttonOk = document.getElementById("delivery-button-ok")
+// payment
+const paymentPopupContainer = document.getElementById("payment-popup-container")
+const paymentPopupButton = document.getElementById("payment-popup-button")
+const paymentCloseButton = document.getElementById("payment-popup-close-button")
+const paymentbuttonOk = document.getElementById("payment-button-ok")
+const cardNumberField = document.getElementById("card-number")
+const cardImg = document.getElementById("card-img")
+
+function toggleDeliveryPopup() {
     deliveryPopupContainer.classList.toggle("show-popup");
 }
 
 function windowOnClick(event) {
     if (event.target === deliveryPopupContainer) {
-        togglePopup();
+        toggleDeliveryPopup()
+    }
+    else if (event.target === paymentPopupContainer) {
+        togglePaymentPopup()
     }
 }
 
+function togglePaymentPopup() {
+    paymentPopupContainer.classList.toggle("show-popup");
+}
+
+
 
 function chooseAddress() {
-    togglePopup()
+    toggleDeliveryPopup()
     if (document.querySelector(".popup-tab-active") === tabIssue) {
         const radio = document.querySelector('input[name="issue-point"]:checked')
         const value = radio.value
@@ -35,11 +52,27 @@ function chooseAddress() {
         addressField.innerText = address
     }
 }
+function chooseCard() {
+    togglePaymentPopup()
+    const radio = document.querySelector('input[name="card"]:checked')
+    const value = radio.value
+    let [cardNumber, cardType] = value.split("$$$")
+    cardNumber = cardNumber.trim()
+    cardType = cardType.trim()
+    cardNumberField.innerText = cardNumber
+    cardImg.src = `images/payment/${cardType}.svg`
+}
 
 
-
-deliveryPopupButton.addEventListener("click", togglePopup)
-deliveryCloseButton.addEventListener("click", togglePopup)
+//delivery
+deliveryPopupButton.addEventListener("click", toggleDeliveryPopup)
+deliveryCloseButton.addEventListener("click", toggleDeliveryPopup)
 deliverybuttonOk.addEventListener("click", chooseAddress)
+
+//payment
+paymentPopupButton.addEventListener("click", togglePaymentPopup)
+paymentCloseButton.addEventListener("click", togglePaymentPopup)
+paymentbuttonOk.addEventListener("click", chooseCard)
+
 
 window.addEventListener("click", windowOnClick)
